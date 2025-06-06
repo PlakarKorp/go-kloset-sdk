040b511959681aef27e93f9bd08549b4d0c0cc0f
Landry Gigant <landry.gigant@epitech.eu>
2025-06-06 14:13:31 +0200

    Importer Fixed with multipleClient

--- sdk/sdk_importer.go
+++ sdk/sdk_importer.go
@@ -8,7 +8,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"io"
-	"log"
+	//"log"
 	"net"
 	"os"
 	"sync"
@@ -40,8 +40,6 @@ type ImporterPluginServer struct {
 	importer       kloset_importer.Importer
 	mu             sync.Mutex // mutex to protect the holdingReaders map
 	holdingReaders map[string]io.ReadCloser
-	scanResults    chan *kloset_importer.ScanResult // this is used to hold the scan results
-	done           chan struct{}
 
 	grpc_importer.UnimplementedImporterServer
 }
@@ -54,116 +52,109 @@ func (plugin *ImporterPluginServer) Info(ctx context.Context, req *grpc_importer
 	}, nil
 }
 
-func (plugin *ImporterPluginServer) Scan(ctx context.Context, req *grpc_importer.ScanRequest) (*grpc_importer.ScanResponse, error) {
+func (plugin *ImporterPluginServer) Scan(req *grpc_importer.ScanRequest, stream grpc_importer.Importer_ScanServer) error {
 	scanResults, err := plugin.importer.Scan()
-	plugin.scanResults = make(chan *kloset_importer.ScanResult) // Create a bidirectional channel
-	go func() {
-		for result := range scanResults {
-			plugin.scanResults <- result
-		}
-		close(plugin.scanResults)
-		close(plugin.done)
-		log.Printf("[SDK] Scan completed, done channel closed\n")
-	}()
 	if err != nil {
-		return nil, fmt.Errorf("failed to start scan: %w", err)
+		//log.Printf("[SDK] Scan error: %v\n", err)
+		return err
 	}
-	return &grpc_importer.ScanResponse{}, nil
-}
 
-func (plugin *ImporterPluginServer) GetRecord(ctx context.Context, req *grpc_importer.GetRecordRequest) (*grpc_importer.GetRecordResponse, error) {
-	log.Printf("[SDK] GetRecord called\n")
-	result := <-plugin.scanResults
-	log.Printf("[SDK] GetRecord result: %v\n", result)
-	if result == nil {
-		return nil, fmt.Errorf("no scan result received")
-	}
-	switch {
-	case result.Record != nil:
-		//if err := stream.Context().Err(); err != nil {
-		//	//log.Printf("[SDK] Scan context error: %v\n", err)
-		//	return err
-		//}
+	for result := range scanResults {
+		//log.Printf("[SDK] Scan result: %v\n", result)
 
-		var xattr *grpc_importer.ExtendedAttribute
-		if result.Record.IsXattr {
-			xattr = &grpc_importer.ExtendedAttribute{
-				Name: result.Record.XattrName,
-				Type: grpc_importer.ExtendedAttributeType(result.Record.XattrType),
+		if err := stream.Context().Err(); err != nil {
+			//log.Printf("[SDK] Scan context error: %v\n", err)
+			return err
+		}
+
+		switch {
+		case result.Record != nil:
+			if err := stream.Context().Err(); err != nil {
+				//log.Printf("[SDK] Scan context error: %v\n", err)
+				return err
 			}
-		}
 
-		//log.Printf("[SDK] Sending scan result for pathname: %s, target: %s, fileinfo: %+v, xattr: %+v\n", result.Record.Pathname, result.Record.Target, result.Record.FileInfo, xattr)
+			var xattr *grpc_importer.ExtendedAttribute
+			if result.Record.IsXattr {
+				xattr = &grpc_importer.ExtendedAttribute{
+					Name: result.Record.XattrName,
+					Type: grpc_importer.ExtendedAttributeType(result.Record.XattrType),
+				}
+			}
 
-		ret := &grpc_importer.GetRecordResponse{
-			Pathname: result.Record.Pathname,
-			Result: &grpc_importer.GetRecordResponse_Record{
-				Record: &grpc_importer.ScanRecord{
-					Target: result.Record.Target,
-					Fileinfo: &grpc_importer.ScanRecordFileInfo{
-						Name:      result.Record.FileInfo.Lname,
-						Size:      result.Record.FileInfo.Lsize,
-						Mode:      uint32(result.Record.FileInfo.Lmode),
-						ModTime:   timestamppb.New(result.Record.FileInfo.LmodTime),
-						Dev:       result.Record.FileInfo.Ldev,
-						Ino:       result.Record.FileInfo.Lino,
-						Uid:       result.Record.FileInfo.Luid,
-						Gid:       result.Record.FileInfo.Lgid,
-						Nlink:     uint32(result.Record.FileInfo.Lnlink),
-						Username:  result.Record.FileInfo.Lusername,
-						Groupname: result.Record.FileInfo.Lgroupname,
-						Flags:     result.Record.FileInfo.Flags,
+			//log.Printf("[SDK] Sending scan result for pathname: %s, target: %s, fileinfo: %+v, xattr: %+v\n", result.Record.Pathname, result.Record.Target, result.Record.FileInfo, xattr)
+
+			ret := &grpc_importer.ScanResponse{
+				Pathname: result.Record.Pathname,
+				Result: &grpc_importer.ScanResponse_Record{
+					Record: &grpc_importer.ScanRecord{
+						Target: result.Record.Target,
+						Fileinfo: &grpc_importer.ScanRecordFileInfo{
+							Name:      result.Record.FileInfo.Lname,
+							Size:      result.Record.FileInfo.Lsize,
+							Mode:      uint32(result.Record.FileInfo.Lmode),
+							ModTime:   timestamppb.New(result.Record.FileInfo.LmodTime),
+							Dev:       result.Record.FileInfo.Ldev,
+							Ino:       result.Record.FileInfo.Lino,
+							Uid:       result.Record.FileInfo.Luid,
+							Gid:       result.Record.FileInfo.Lgid,
+							Nlink:     uint32(result.Record.FileInfo.Lnlink),
+							Username:  result.Record.FileInfo.Lusername,
+							Groupname: result.Record.FileInfo.Lgroupname,
+							Flags:     result.Record.FileInfo.Flags,
+						},
+						FileAttributes: result.Record.FileAttributes,
+						Xattr:          xattr,
 					},
-					FileAttributes: result.Record.FileAttributes,
-					Xattr:          xattr,
 				},
-			},
-		}
+			}
 
-		if result.Record.FileInfo.Mode()&os.ModeDir != 0 {
-			//log.Printf("[SDK] Directory detected, not adding to flying scan: %s\n", result.Record.Pathname)
-		} else {
-			log.Printf("[SDK] stroring record reader\n")
-			plugin.mu.Lock()
-			defer plugin.mu.Unlock()
-			plugin.holdingReaders[result.Record.Pathname] = result.Record.Reader //THE PROGRAM BLOCK HERE
-			log.Printf("[SDK] Added reader for pathname: %s, total readers: %d\n", result.Record.Pathname, len(plugin.holdingReaders))
-		}
-		return ret, nil
+			if result.Record.FileInfo.Mode()&os.ModeDir != 0 {
+				//log.Printf("[SDK] Directory detected, not adding to flying scan: %s\n", result.Record.Pathname)
+			} else {
+				//log.Printf("[SDK] stroring record reader\n")
+				plugin.mu.Lock()
+				plugin.holdingReaders[result.Record.Pathname] = result.Record.Reader //THE PROGRAM BLOCK HERE
+				//log.Printf("[SDK] Added reader for pathname: %s, total readers: %d\n", result.Record.Pathname, len(plugin.holdingReaders))
+				plugin.mu.Unlock()
+			}
+			if err := stream.Send(ret); err != nil {
+				//log.Printf("[SDK] Error sending scan response for pathname %s: %v\n", result.Record.Pathname, err)
+				return err
+			}
 
-	case result.Error != nil:
-		ret := &grpc_importer.GetRecordResponse{
-			Pathname: result.Error.Pathname,
-			Result: &grpc_importer.GetRecordResponse_Error{
-				Error: &grpc_importer.ScanError{
-					Message: result.Error.Err.Error(),
+		case result.Error != nil:
+			ret := &grpc_importer.ScanResponse{
+				Pathname: result.Error.Pathname,
+				Result: &grpc_importer.ScanResponse_Error{
+					Error: &grpc_importer.ScanError{
+						Message: result.Error.Err.Error(),
+					},
 				},
-			},
+			}
+			//log.Printf("[SDK] Sending scan error for pathname: %s, error: %v\n", result.Error.Pathname, result.Error.Err)
+			if err := stream.Send(ret); err != nil {
+				//log.Printf("[SDK] Error sending scan error for pathname %s: %v\n", result.Error.Pathname, err)
+				return err
+			}
+		default:
+			//log.Printf("[SDK] Unknown scan result type for pathname: %s\n", result.Record.Pathname)
+			panic("?? unknown result type ??")
 		}
-		return ret, nil
-	default:
-		//log.Printf("[SDK] Unknown scan result type for pathname: %s\n", result.Record.Pathname)
-		panic("?? unknown result type ??")
 	}
-}
-
-func (plugin *ImporterPluginServer) ScanDone(ctx context.Context, req *grpc_importer.ScanDoneRequest) (*grpc_importer.ScanDoneResponse, error) {
-	select {
-	case <-plugin.done:
-		return &grpc_importer.ScanDoneResponse{Done: true}, nil
-	default:
-		return &grpc_importer.ScanDoneResponse{Done: false}, nil
-	}
+	//log.Printf("[SDK] Scan completed, closing stream\n")
+	return nil
 }
 
 func (plugin *ImporterPluginServer) Open(req *grpc_importer.OpenRequest, stream grpc_importer.Importer_OpenServer) error {
 	pathname := req.Pathname
 
-	log.Printf("[SDK] Opening pathname: %s\n", pathname)
+	//log.Printf("[SDK] Opening pathname: %s\n", pathname)
 
 	plugin.mu.Lock()
 	defer plugin.mu.Unlock()
 	reader, ok := plugin.holdingReaders[pathname]
+	//log.Printf("[SDK] Holding readers: %d\n", len(plugin.holdingReaders))
 	if !ok {
 		return fmt.Errorf("no reader for pathname %s", pathname)
 	}
@@ -179,7 +170,7 @@ func (plugin *ImporterPluginServer) Open(req *grpc_importer.OpenRequest, stream 
 			}
 		}
 		if err == io.EOF {
-			log.Printf("[SDK] End of file reached for pathname: %s\n", pathname)
+			//log.Printf("[SDK] End of file reached for pathname: %s\n", pathname)
 			break
 		}
 		if err != nil {
@@ -192,17 +183,17 @@ func (plugin *ImporterPluginServer) Open(req *grpc_importer.OpenRequest, stream 
 func (plugin *ImporterPluginServer) Close(ctx context.Context, req *grpc_importer.CloseRequest) (*grpc_importer.CloseResponse, error) {
 	pathname := req.Pathname
 
-	log.Printf("[SDK] Closing pathname: %s\n", pathname)
+	//log.Printf("[SDK] Closing pathname: %s\n", pathname)
 
 	plugin.mu.Lock()
 	defer plugin.mu.Unlock()
 	if reader, ok := plugin.holdingReaders[pathname]; ok {
 		if err := reader.Close(); err != nil {
-			log.Printf("[SDK] Error closing reader for pathname %s: %v\n", pathname, err)
+			//log.Printf("[SDK] Error closing reader for pathname %s: %v\n", pathname, err)
 			return nil, err
 		}
 		delete(plugin.holdingReaders, pathname)
-		log.Printf("[SDK] Closed reader for pathname: %s\n", pathname)
+		//log.Printf("[SDK] Closed reader for pathname: %s\n", pathname)
 	}
 	//} else {
 	//log.Printf("[SDK] No reader found for pathname: %s\n", pathname)
@@ -213,22 +204,21 @@ func (plugin *ImporterPluginServer) Close(ctx context.Context, req *grpc_importe
 }
 
 func RunImporter(imp kloset_importer.Importer) error {
-	file := os.NewFile(3, "grpc-conn")
-	if file == nil {
-		return fmt.Errorf("failed to get file descriptor for fd 3")
-	}
-	defer file.Close()
+	sockPath := "/tmp/importer.sock"
+	_ = os.Remove(sockPath) // cleanup in case it exists
 
-	conn, err := net.FileConn(file)
+	listener, err := net.Listen("unix", sockPath)
 	if err != nil {
-		return fmt.Errorf("failed to convert fd to net.Conn: %w", err)
+		return fmt.Errorf("failed to create Unix socket: %w", err)
 	}
-
-	listener := &singleConnListener{conn: conn}
+	defer os.Remove(sockPath)
 
 	server := grpc.NewServer()
-
-	grpc_importer.RegisterImporterServer(server, &ImporterPluginServer{importer: imp, holdingReaders: make(map[string]io.ReadCloser, 1000), done: make(chan struct{})})
+	grpc_importer.RegisterImporterServer(server, &ImporterPluginServer{
+		importer:       imp,
+		holdingReaders: make(map[string]io.ReadCloser),
+	})
+	//log.Printf("[SDK] gRPC server listening on unix://%s\n", sockPath)
 
 	if err := server.Serve(listener); err != nil {
 		return err
