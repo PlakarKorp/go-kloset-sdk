ee4315e943a8aa9b86dc46c92ac0b0f5015af4df
Gilles Chehade <gilles@poolp.org>
2025-08-26 23:44:39 +0200

    Revert "remove dependency to kloset"
    This reverts commit 629493173ffadf44fe194c406b82eb47b0fd0f20.

--- storage.go
+++ storage.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/PlakarKorp/go-kloset-contracts/objects"
+	"github.com/PlakarKorp/kloset/kcontext"
+	"github.com/PlakarKorp/kloset/objects"
 
-	kloset_storage "github.com/PlakarKorp/go-kloset-contracts/storage"
 	kloset_grpc_storage "github.com/PlakarKorp/integration-grpc/storage"
 	grpc_storage "github.com/PlakarKorp/integration-grpc/storage/pkg"
+	kloset_storage "github.com/PlakarKorp/kloset/storage"
 
 	"google.golang.org/grpc"
 )
@@ -38,7 +39,7 @@ func (plugin *storagePluginServer) Init(ctx context.Context, req *grpc_storage.I
 
 // Create creates a new storage with given configuration.
 func (plugin *storagePluginServer) Create(ctx context.Context, req *grpc_storage.CreateRequest) (*grpc_storage.CreateResponse, error) {
-	err := plugin.storage.Create(ctx, req.Config)
+	err := plugin.storage.Create(kcontext.NewKContext(), req.Config)
 	if err != nil {
 		return nil, err
 	}
